5f3229e5656e1419bcebc389c7718d0e9f9de255
RoDNes-root <beytullahrodnes@gmail.com>
2022-09-09 14:28:40 +0300

    localstorageuser

---
+++ javascript-training-projects/localstorageuse/js/form.js
@@ -0,0 +1,66 @@
+let userFormDOM = document.querySelector('#userForm');
+userFormDOM.addEventListener('submit', formHandler) // formHandler is a function
+const button = document.querySelector('#delete');
+
+
+function formHandler(event){
+    event.preventDefault()
+    const USER_NAME = document.querySelector('#username')
+    const NOT = document.querySelector('#not')
+    if(USER_NAME.value && NOT.value){
+        addItem(USER_NAME.value, NOT.value)
+        let userStatus = {userName: USER_NAME.value, NOT: NOT.value}
+        localStorage.setItem('user', JSON.stringify(userStatus))
+        USER_NAME.value = '' // clear the input
+        NOT.value = '' // clear the input
+        Swal.fire({
+            icon: 'success',
+            title: 'Not Bilgisi eklendi',
+            showConfirmButton: false,
+            timer: 1200
+          })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Hata',
+            text: 'Lütfen bilgileri boş bırakmayın !!',
+          })
+    }
+    
+}
+
+let userListDOM = document.querySelector('#userList')
+
+if (localStorage.length > 0) {
+    let user = JSON.parse(localStorage.getItem('user'))
+    let liDOM = document.createElement('li')
+    userFormDOM =  liDOM.innerHTML = `${user.userName} 
+    <span class="badge bg-primary rounded-pill">${user.NOT}</span>
+    `
+    liDOM.classList.add(
+        'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
+    userListDOM.appendChild(liDOM)
+
+}
+
+
+const addItem = (userName, not) => {
+    let liDOM = document.createElement('li')
+    liDOM.innerHTML = `${userName} 
+    <span class="badge bg-primary rounded-pill">${not}</span>
+    `
+    liDOM.classList.add(
+        'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
+    userListDOM.appendChild(liDOM)
+}
+
+
+button.addEventListener('click', function () {
+    localStorage.clear();
+    Swal.fire({
+        icon: 'success',
+        title: 'Local Storage Temizlendi',
+        showConfirmButton: false,
+        timer: 1200
+      })
+  });
